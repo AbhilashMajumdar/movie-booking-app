3a9f9dc870f3891e663044840d701658069fc3a6
Abhilash <abhilashmajumdar306@gmail.com>
2026-01-19 14:42:09 +0530

    "Cors issue fixed"

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import path from "path";
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://movie-booking-app-by-abhilash-majumdar.onrender.com", credentials: true }));
 app.use(cookieParser());
 
 const _dirname = path.resolve();
--- frontend/src/constants/index.jsx
+++ frontend/src/constants/index.jsx
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:5000/api/v1/moviebooking/"
+export const BASE_URL = "https://movie-booking-app-by-abhilash-majumdar.onrender.com/api/v1/moviebooking/"
 
 export const SEATS_PER_ROW = 10;
 export const seatRows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
